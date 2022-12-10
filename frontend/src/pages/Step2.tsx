3d469e26265daa90b59b5c5de58461231f3fe007
rChaoz <catalin_trand2@yahoo.com>
2022-12-10 16:57:30 +0200

    Fix warnings

--- frontend/src/pages/Step2.tsx
+++ frontend/src/pages/Step2.tsx
@@ -1,21 +1,14 @@
-import {useLoaderData} from "react-router-dom";
-import {Text, Checkbox, createStyles, Flex, Loader, TextInput, Title} from "@mantine/core";
+import {Text, TextInput, Title} from "@mantine/core";
 import React from "react";
 
-const useStyles = createStyles(theme => ({
-}))
 
 interface Step2Props {
-
 }
 
 export function Step2({}: Step2Props) {
-    const {classes} = useStyles()
-
     return (<div>
-        <Title order={2} align={"center"} py={"sm"}>In ce locatie ne vizitezi?</Title>
-        <Text pt={"xs"} pb={"md"}>Cauta unitatea BCR unde programezi vizita</Text>
-        <TextInput label="Cauta unitatea" placeholder="Nume unitate / Adresa / Zona"/>
-
+        <Title order={2} align={"center"} py={"sm"}>În ce locație ne vizitezi?</Title>
+        <Text pt={"xs"} pb={"md"}>Caută unitatea BCR unde programezi vizita:</Text>
+        <TextInput label="Caută unitatea" placeholder="Nume unitate / Adresă / Zonă"/>
     </div>)
 }
