0065551ac392df8d8f779b932700eb8ef432ab62
rChaoz <catalin_trand2@yahoo.com>
2022-12-10 14:27:10 +0200

    Add Step shell

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,26 @@
 import {MantineProvider} from "@mantine/core";
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes} from "react-router-dom";
-import {Step1, Step1Load} from "./pages/step1";
+import {Step1, Step1Load} from "./pages/Step1";
 import React from "react";
+import Step from "./pages/Step";
 
 const routes = (<>
-    <Route path={"/"} element={<Step1 unitate={"unitate"}/>} loader={Step1Load}/>
-    <Route path={"/step2"}/>
+    <Route path={"/step"} element={<Step/>}>
+        <Route index={true} element={<Step1 unitate={"ceva"}/>} loader={Step1Load}/>
+    </Route>
 </>)
 
 const router = createBrowserRouter(createRoutesFromElements(routes))
 
 export default function App() {
-    return (<MantineProvider>
+    return (<MantineProvider withNormalizeCSS withGlobalStyles theme={{
+        colorScheme: "light",
+        fontFamily: "'Montserrat', sans-serif",
+        headings: {
+            fontFamily: "'Montserrat', sans-serif",
+        },
+        primaryColor: "blue",
+    }}>
         <RouterProvider router={router}/>
     </MantineProvider>)
 }
---
+++ frontend/src/pages/Step.tsx
@@ -0,0 +1,47 @@
+import {Outlet} from "react-router-dom";
+import {AppShell, Button, Center, createStyles, Flex, Header, Progress, Text, Title} from "@mantine/core";
+import {useState} from "react";
+
+const useStyles = createStyles(theme => ({
+    header: {
+        backgroundColor: "#21416c",
+        color: theme.white,
+    },
+    bar: {
+        height: 80,
+    },
+    step: {
+        backgroundColor: "#f4f6fa",
+    },
+}))
+
+interface StepProps {
+
+}
+
+export default function Step({}: StepProps) {
+    const [step, setStep] = useState(1)
+
+    return <AppShell header={<StepHeader step={step}/>}>
+        <Outlet/>
+    </AppShell>
+}
+
+interface StepHeaderProps {
+    step: number,
+}
+
+function StepHeader({step}: StepHeaderProps) {
+    const {classes} = useStyles()
+    return <Header height={80} className={classes.header}>
+        <Center className={classes.bar}>
+            <Title>Programare vizita la BCR</Title>
+        </Center>
+        <Center py={"md"} className={classes.step}>
+            <Flex direction={"column"}>
+                <Text color={"#5c7999"}>Pasul {step} - Alege scopul vizitei</Text>
+                <Progress value={step / 7 * 100} color={"#21416c"}/>
+            </Flex>
+        </Center>
+    </Header>
+}
