39dfa76a6d9ba856b98c20bb7576a2dd030545fc
Andi Koleci <andikoleci@yahoo.com>
2022-12-10 15:24:05 +0200

    Implement UI for step1

--- frontend/src/pages/Step1.tsx
+++ frontend/src/pages/Step1.tsx
@@ -1,6 +1,9 @@
 import {useLoaderData} from "react-router-dom";
-import {Checkbox} from "@mantine/core";
-import App from "../App"
+import {Button, Checkbox, createStyles, Flex} from "@mantine/core";
+import React from "react";
+
+const useStyles = createStyles(theme => ({
+}))
 
 interface Step1Props {
     unitate: String
@@ -12,14 +15,39 @@ interface Step1Data {
 }
 
 export function Step1({unitate, test}: Step1Props) {
+    const {classes} = useStyles()
     const date_unitate = useLoaderData() as Step1Data
 
     const zile = date_unitate.zile
 
     return (<div>
-        <p>Buna</p>
-        <Checkbox label={"Hhe hihi"}/>
-        {zile.map((zi: number) => <button>{zi}</button>)}
+        <Flex
+            mih={50}
+            bg="white"
+            gap="xs"
+            justify="flex-start"
+            align="stretch"
+            direction="column"
+            wrap="wrap"
+        >
+            <Button variant={"outline"}>Depunere sau retragere de bani</Button>
+            <Button variant={"outline"}>Plata rata credit</Button>
+            <Button variant={"outline"}>Operatiune fara numerar</Button>
+            <Button variant={"outline"}>Deschidere cont curent</Button>
+            <Button variant={"outline"}>Deschidere cont minori</Button>
+            <Button variant={"outline"}>Deschidere conturi refugiati</Button>
+            <Button variant={"outline"}>Suport utilizare aplicatie George</Button>
+            <Button variant={"outline"}>Diagnostic financiar gratuit</Button>
+            <Button variant={"outline"}>Credit de nevoi personale</Button>
+            <Button variant={"outline"}>Credit ipotecar</Button>
+            <Button variant={"outline"}>Economisire</Button>
+            <Button variant={"outline"}>Contracte BpL</Button>
+            <Button variant={"outline"}>Investitii - subscriere</Button>
+            <Button variant={"outline"}>Investitii - rascumparare</Button>
+            <Button variant={"outline"}>Asigurare</Button>
+            <Button variant={"outline"}>Pensie privata</Button>
+
+        </Flex>
     </div>)
 }
 
