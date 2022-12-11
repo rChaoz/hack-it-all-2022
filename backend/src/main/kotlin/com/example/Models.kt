533003b277e6d7d1fdb7b68d6ce6beb53626c459
rChaoz <catalin_trand2@yahoo.com>
2022-12-11 02:01:04 +0200

    Add Appointment model

--- backend/src/main/kotlin/com/example/Models.kt
+++ backend/src/main/kotlin/com/example/Models.kt
@@ -1,2 +1,11 @@
 package com.example
 
+data class Appointment(
+    val name: String,
+    val branch: String,
+    val purpose: String,
+    val dateAndTime: String,
+    val address: String,
+    val calendarLink: String,
+    val cancelAppointment: String,
+)
--- backend/src/main/kotlin/com/example/plugins/Routing.kt
+++ backend/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,7 +1,6 @@
 package com.example.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
