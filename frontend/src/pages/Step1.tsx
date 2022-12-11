4299e4dd1011a7718ebc906682dfc39fc8e77424
rChaoz <catalin_trand2@yahoo.com>
2022-12-11 09:55:15 +0200

    fix step1

--- frontend/src/pages/Step1.tsx
+++ frontend/src/pages/Step1.tsx
@@ -27,22 +27,22 @@ export function Step1({}: Step1Props) {
         <Title order={2} align={"center"}>Despre ce vrei să vorbim?</Title>
         <Text align={"center"} pb={"md"}>Alege un motiv pentru care programezi vizita în unitate:</Text>
         <Flex gap="xs" align="stretch" direction="column" mx={context.smallScreen ? 0 : 100}>
-            <RadioButton name={"depunere_retragere"} checked={checked} onChange={onChangeHandler}>Depunere sau retragere de bani</RadioButton>
-            <RadioButton name={"plata_rata_credit"} checked={checked} onChange={onChangeHandler}>Plată rată credit</RadioButton>
-            <RadioButton name={"operatiune_fara_numerar"} checked={checked} onChange={onChangeHandler}>Operațiune fără numerar</RadioButton>
-            <RadioButton name={"deschidere_cont_curent"} checked={checked} onChange={onChangeHandler}>Deschidere cont curent</RadioButton>
-            <RadioButton name={"deschidere_cont_minori"} checked={checked} onChange={onChangeHandler}>Deschidere cont minori</RadioButton>
-            <RadioButton name={"deschidere_cont_refugiati"} checked={checked} onChange={onChangeHandler}>Deschidere conturi refugiați</RadioButton>
-            <RadioButton name={"suport_aplicatie"} checked={checked} onChange={onChangeHandler}>Suport utilizare aplicație George</RadioButton>
-            <RadioButton name={"diagnostic_financiar"} checked={checked} onChange={onChangeHandler}>Diagnostic financiar gratuit</RadioButton>
-            <RadioButton name={"credit_nevoi"} checked={checked} onChange={onChangeHandler}>Credit de nevoi personale</RadioButton>
-            <RadioButton name={"credit_ipotecar"} checked={checked} onChange={onChangeHandler}>Credit ipotecar</RadioButton>
-            <RadioButton name={"economisire"} checked={checked} onChange={onChangeHandler}>Economisire</RadioButton>
-            <RadioButton name={"contracte_bpl"} checked={checked} onChange={onChangeHandler}>Contracte BpL</RadioButton>
-            <RadioButton name={"investitii_subscriere"} checked={checked} onChange={onChangeHandler}>Investiții - subscriere</RadioButton>
-            <RadioButton name={"investitii_rascumparare"} checked={checked} onChange={onChangeHandler}>Investiții - răscumpărare</RadioButton>
-            <RadioButton name={"asigurare"} checked={checked} onChange={onChangeHandler}>Asigurare</RadioButton>
-            <RadioButton name={"pensie"} checked={checked} onChange={onChangeHandler}>Pensie privată</RadioButton>
+            <RadioButton name={"Depunere sau retragere de bani"} checked={checked} onChange={onChangeHandler}>Depunere sau retragere de bani</RadioButton>
+            <RadioButton name={"Plată rată credit"} checked={checked} onChange={onChangeHandler}>Plată rată credit</RadioButton>
+            <RadioButton name={"Operațiune fără numerar"} checked={checked} onChange={onChangeHandler}>Operațiune fără numerar</RadioButton>
+            <RadioButton name={"Deschidere cont curent"} checked={checked} onChange={onChangeHandler}>Deschidere cont curent</RadioButton>
+            <RadioButton name={"Deschidere cont minori"} checked={checked} onChange={onChangeHandler}>Deschidere cont minori</RadioButton>
+            <RadioButton name={"Deschidere conturi refugiați"} checked={checked} onChange={onChangeHandler}>Deschidere conturi refugiați</RadioButton>
+            <RadioButton name={"Suport utilizare aplicație George"} checked={checked} onChange={onChangeHandler}>Suport utilizare aplicație George</RadioButton>
+            <RadioButton name={"Diagnostic financiar gratuit"} checked={checked} onChange={onChangeHandler}>Diagnostic financiar gratuit</RadioButton>
+            <RadioButton name={"Credit de nevoi personale"} checked={checked} onChange={onChangeHandler}>Credit de nevoi personale</RadioButton>
+            <RadioButton name={"Credit ipotecar"} checked={checked} onChange={onChangeHandler}>Credit ipotecar</RadioButton>
+            <RadioButton name={"Economisire"} checked={checked} onChange={onChangeHandler}>Economisire</RadioButton>
+            <RadioButton name={"Contracte BpL"} checked={checked} onChange={onChangeHandler}>Contracte BpL</RadioButton>
+            <RadioButton name={"Investiții - subscriere"} checked={checked} onChange={onChangeHandler}>Investiții - subscriere</RadioButton>
+            <RadioButton name={"Investiții - răscumpărare"} checked={checked} onChange={onChangeHandler}>Investiții - răscumpărare</RadioButton>
+            <RadioButton name={"Asigurare"} checked={checked} onChange={onChangeHandler}>Asigurare</RadioButton>
+            <RadioButton name={"Pensie privată"} checked={checked} onChange={onChangeHandler}>Pensie privată</RadioButton>
         </Flex>
     </>)
 }
